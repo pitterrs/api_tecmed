30790779951a57816f1b89bd5497637dd280b385
Pitter <pitterrs@gmail.com>
2023-07-21 10:36:35 -0300

    alterando banco de dados

--- connection.js
+++ connection.js
@@ -10,11 +10,15 @@ import mysql from "mysql";
 export const connection = mysql.createPool({
     connectionLimit : 100,
     // host: "br104.hostgator.com.br",
-    host: "192.185.210.220",
-    user: "pitter45_robson",
-    password: "oAm2FUQGHdG8W",
+    // host: "192.185.210.220",
+    host: "192.185.221.174",
+    // user: "pitter45_robson",
+    user: "tecmed48_robson",
+    // password: "oAm2FUQGHdG8W",
+    password: "PACLgXDAKDan",
     port: 3306,
-    database: "pitter45_tecmed",
+    // database: "pitter45_tecmed",
+    database: "tecmed48_tecmed",
     multipleStatements: true,
     timezone: 'BRT'
 })
